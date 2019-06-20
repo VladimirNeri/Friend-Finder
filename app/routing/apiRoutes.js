50f9f5892b28941b005c080695d31acd0802c294
obscura101 <vlad.neri@gmail.com>
2019-06-19 22:37:09 -0700

    added base code

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,15 @@
+// Activity 16
+var friendData = require("../data/friends.js");
+
+module.exports = function(app) {
+    
+    app.get("/api/friends", function(req, res) {
+        res.json(friendData);
+    });
+
+    app.post("/api/friends", function(req, res) {
+
+        // Need JS Here
+        
+    });
+};
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,17 +1,17 @@
+// Dependencies - Activity 16 
+
 var path = require('path');
 var express = require("express");
 
+// Routing - Activity 16
 module.exports = function (app) {
 
-    app.use(express.static(__dirname + '/../public'));
-
-    //Sends user to survey.html
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "/../public/survey.html"));
     });
 
-    //Sends user to home page if they go to any url path that isn't already defined
     app.use(function (req, res) {
         res.sendFile(path.join(__dirname, "/../public/home.html"));
     });
+    
 }

d0668a6cfd98a460ed248e886e23e8a7db500b09
obscura101 <vlad.neri@gmail.com>
2019-06-21 09:37:45 -0700

    draft

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,6 +10,29 @@ module.exports = function(app) {
     app.post("/api/friends", function(req, res) {
 
         // Need JS Here
-        
+        var potentialFriend = req.body;
+        var overallDiff = [];
+        var potentialMatches = [];
+   
+        if (friendData.length >= 1) {
+            friendData.forEach(function(friend) {
+                var difference = 0;
+                var differenceTemp = parseInt(friend.total) - parseInt(potentialFriend.total);
+                difference = difference + Math.abs(differenceTemp);
+                overallDiff.push(difference);
+
+            });
+
+            var minDiff = Math.min.apply(Math, overallDiff);
+            for (var i = 0; i < overallDiff.length; i++) {
+                if (overallDiff[i] === minDiff) {
+                    potentialMatches.push(friendData[i]);
+                }
+            }
+            res.json(potentialMatches);
+
+        }
+
+        friendData.push(potentialFriend);
     });
 };

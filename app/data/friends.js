9a2460ed2f0cf99d53230c386a653ab5f568f02c
obscura101 <vlad.neri@gmail.com>
2019-06-21 17:31:40 -0700

    fixed friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,17 @@
 var friends = [{
-    name: "Hailey",
-    photo: "https://upload.wikimedia.org/wikipedia/commons/d/d4/CH_cow_2_cropped.jpg",
-    scores: [
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5,
-        5
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
     ]
 }];
 

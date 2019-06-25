8e44ac37b07535cf633d18543fc0020651b8d47b
obscura101 <vlad.neri@gmail.com>
2019-06-24 21:27:41 -0700

    Final

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
 
 // Router
 require("./app/routing/apiRoutes")(app);
